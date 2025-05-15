c250827deab4369ea19dc2590e4c90875edc66f3
Minh Hoang <hoangminhng208@>
2025-05-15 16:35:53 +0000

    perf: optimize io performence

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from src.utils import print_top_documents
 # 1. Tạo index
 index, doc_table, term_table = create_index('data', 'stoplist.txt')
 
+print("index",index)
+print("doc_table",doc_table)
+print("term_table",term_table)
+
 # 2. Tìm theo 1 từ
 print("Top documents for word='cat':")
 results = find_single('cat', weight=3, N=3, inverted_index=index, doc_table=doc_table)
